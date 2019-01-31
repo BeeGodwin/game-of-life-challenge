b747be4a686bb8934d8c5fbe0ac552d02644ad70
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2019-01-31 14:45:07 +0000

    unit test for bounds

--- test_LifeGame.py
+++ test_LifeGame.py
@@ -12,3 +12,14 @@ class TestLifeGame(object):
         game = LifeGame([(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)])
         assert game.neighbours((0, 0)) == 8
         assert game.neighbours((2, 0)) == 3
+
+    def test_bounds(self):
+
+        game = LifeGame([])
+        assert game.bounds == (0, 0, 0, 0)
+
+        game = LifeGame([(0, 0)])
+        assert game.bounds == (-1, -1, 3, 3)
+
+        game = LifeGame([(-1, -1), (1, 1)])
+        assert game.bounds == (-2, -2, 5, 5)
