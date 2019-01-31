b62c8707d980f574df0719ac8060ad05b2fd6ee6
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2019-01-31 14:10:09 +0000

    Rethink high level design of LifeGame class.

--- life.py
+++ life.py
@@ -3,26 +3,18 @@ class LifeGame:
 
     This board is an infinite Cartesian plane starting at 0,0.
 
-    Each location x, y, where x and y are ints, contains True for a living cell and False for an empty cell.
-    This class maintains a two dimensional list to represent the board. The x, y range of the list describes the bounds
-    the area containing living cells. This class will adjust storage as needed to cope with larger bounds."""
+    This class has a set, live_set,  of live cells as (x, y) tuples, and takes a list of (x, y) tuples in its
+    constructor as its intial list of living cells.
 
-    # set min board & init buffer
-    def __init__(self):
-        self.board = [[False]]  # a one-cell board
-        self.buffer = [[False]]
-        self.origin = (0, 0)  # origin is an (x, y) tuple pointing to the cell in board that represents the origin
-        self.buffer_origin = (0, 0)  # when an iteration causes the board to grow, we need the old and new origins.
+    Each time iterate is called, find the bounds of the area of living cells and pad by one.
 
-    def get_cell_at(self, x, y):
-        """If the location is on the board, returns True for a living cell or False for an empty cell.
-        If the location is not on the board, returns False."""
-        pass
-
-    def set_cell_at(self, x, y, live):
-        """If the location is on the board, sets this cell in the buffer to the True/False value of live.
-        If the location is not on the board, enlarges the buffer appropriately, modifies buffer_origin appropriately."""
-        pass
+    For each cell in this plane, compute its 8 neighbouring (x, y) tuples and determine how many living
+    neighbours it has. Living cells survive with either 2 or 3 neighbours. Cells are created when an empty cell
+    has exactly three neighbours.
+    """
+    def __init__(self, cells):
+        """cells is a list of (x, y) tuples describing living cell locations."""
+        self.liveCells = set(cells)
 
     def iterate(self):
         """Loop through the whole board applying the Game of Life tests to each cell, and copying the results into the
@@ -30,10 +22,4 @@ class LifeGame:
         can still have three neighbours."""
         pass
 
-    def grow_buffer(self):
-        """Determines the bounds of the area of live cells and pads the buffer by one cell in all directions"""
-        pass
 
-    def copy_buffer(self):
-        """Crops the buffer of excess cells and copies it to the board."""
-        pass
