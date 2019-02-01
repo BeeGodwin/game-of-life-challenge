57c2ddc6622b1aae6651140e05c21b59337d944e
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2019-02-01 16:13:17 +0000

    main iterate/quit loop works, stub for display_board

--- life_main.py
+++ life_main.py
@@ -1,5 +1,28 @@
+from life import LifeGame
+
+
+def display_board(life_game):
+    print(life_game.live_cells)
+
+
 def main():
-    pass
+    # get a list of tuples from the user
+    print("Welcome to LifeGame")
+    live = eval(input("Please enter a comma-separated list of integer 2-tuples"))
+
+    game = LifeGame(live)  # unsafe! Check the input first.
+
+    done = False
+
+    while not done:
+        display_board(game)
+        choice = input("Enter to iterate, q to quit")
+        if len(choice) > 0 and choice.capitalize()[0] == "Q":
+            done = True
+            continue
+        game.iterate()
+
+    print("Bye!")
 
 
 if __name__ == "__main__":
